14fed36f3c0488182470404e22f62688d78dc11d
madam007 <adamsetiadijr07@gmail.com>
2023-10-25 10:38:53 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,70 @@
+import os
+from os.path import join,dirname
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from datetime import datetime
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+DB_COLLECTION =  os.environ.get("DB_COLLECTION")
+
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME][DB_COLLECTION]
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/diary', methods=["GET"])
+def showDiary():
+    articles = list(db.find({}, {"_id":False}))
+    # db.delete_many({})
+    return jsonify({
+        "articles": articles,
+    })
+
+@app.route('/diary', methods=["POST"])
+def SaveDiary():
+    today = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
+    file = request.files.get('file_give')
+    profile = request.files.get('profile_give')
+    title_receive = request.form.get('title_give')
+    author_receive = request.form.get('author_give')
+    descrip_receive = request.form.get('description_give')
+    
+    # File response
+    extention = file.filename.split(".")[-1]
+    filename =f"static/img/public/galery/post-{today}.{extention}"
+    file.save(filename)
+
+    # Profile response
+    extention = profile.filename.split(".")[-1]
+    profile_filename =f"static/img/public/profile/profile-{today}.{extention}"
+    profile.save(profile_filename)
+
+    doc = {
+        'img' : filename,
+        "profile" : profile_filename,
+        'author': author_receive,
+        'title': title_receive,
+        'description': descrip_receive
+    }
+    db.insert_one(doc)
+    return jsonify({
+        "msg": "Succes received all Save"
+    })
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
+ 
---
+++ static/js/script.js
@@ -0,0 +1,159 @@
+// Deklarasi
+const customAlert = new CustomAlert();
+
+$(document).ready(function () {
+  // alert("Hello!")
+  cekTheme();
+  listing();
+  bsCustomFileInput.init();
+  // Theme Toggle
+  $("#theme-toggle").click(function () {
+    // Get the current theme state
+    const isDarkThemeEnabled = $("body").hasClass("dark");
+
+    // Toggle the theme state
+    $("body").toggleClass("dark");
+
+    // Update the local storage preference
+    localStorage.setItem("pref-theme", isDarkThemeEnabled ? "dark" : "light");
+  });
+
+
+  // Initialize tooltips
+  $('[data-bs-toggle="tooltip"]').tooltip();
+});
+
+// Function API
+const listing = () => {
+  $.ajax({
+    type: "GET",
+    url: "/diary",
+    success: function (response) {
+      const id = $("#cards-box");
+      const { articles } = response;
+      console.log(articles);
+      articles.forEach((element) => {
+        const { img, profile, author,title, description } = element;
+        const temp_html = `
+        <div class="col">
+        <div class="card h-100" title="${author}">
+          <img src="../${img}" class="card-img-top" alt="${author}">
+          <div class="card-body">
+            <h5 class="card-title">${title}</h5>
+            <p class="card-text">${description}</p>
+          </div>
+          <div class="card-footer">
+          <div class="d-flex flex-wrap justify-content-between align-items-center">
+          <small class="text-time">Last updated 3 mins ago</small>
+          <img src="../${profile}" class="image" alt="adam">
+          </div>
+          </div>
+        </div>
+      </div>
+
+        `;
+        id.append(temp_html);
+      });
+    },
+  });
+};
+
+function posting() {
+  const getTitle = $("#img-title").val();
+  const getProfile = $("#images-profile").prop("files")[0];
+  const getAuthors = $("#author").val();
+  const getfile = $("#images").prop("files")[0];
+  const getDescription = $("#img-description").val();
+  if (!getTitle)
+    return customAlert.alert("Your Title is undefined.", "Hi, There");
+  if (!getDescription)
+    return customAlert.alert("Your Description is undefined.", "Hi, There");
+
+  let form_data = new FormData();
+  form_data.append("file_give", getfile);
+  form_data.append("profile_give", getProfile);
+  form_data.append("author_give", getAuthors);
+  form_data.append("title_give", getTitle);
+  form_data.append("description_give", getDescription);
+
+  $.ajax({
+    type: "POST",
+    url: "/diary",
+    data: form_data,
+    contentType: false,
+    processData: false,
+    success: function (response) {
+      console.log(response);
+      window.location.reload();
+    },
+  });
+}
+
+// Komponents
+const cekTheme = () => {
+  // Get the preferred theme from local storage
+  const prefTheme = localStorage.getItem("pref-theme");
+
+  // Apply the preferred theme
+  if (prefTheme === "dark") {
+    $("body").addClass("dark");
+  } else if (prefTheme === "light") {
+    $("body").removeClass("dark");
+  } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    $("body").addClass("dark");
+  }
+
+};
+
+function CustomAlert() {
+  this.alert = function (message, title) {
+    $("#content-alert").html(`
+      <div id="dialogoverlay"></div>
+      <div id="dialogbox" class="slit-in-vertical">
+        <div>
+          <div id="dialogboxhead"></div>
+          <div id="dialogboxbody"></div>
+          <div id="dialogboxfoot">
+            <button id="btn-ok" class="pure-material-button-contained active">OK</button>
+          </div>
+        </div>
+      </div>
+    `);
+
+    let dialogoverlay = $("#dialogoverlay");
+    let dialogbox = $("#dialogbox");
+
+    let winH = window.innerHeight;
+    dialogoverlay.css("height", winH + "px");
+
+    dialogbox.css("top", "100px");
+
+    dialogoverlay.css("display", "block");
+    dialogbox.css("display", "block");
+
+    $("#dialogboxhead").css("display", "block");
+
+    if (!title) {
+      $("#dialogboxhead").css("display", "none");
+    } else {
+      $("#dialogboxhead").html(
+        '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> ' + title
+      );
+    }
+    $("#dialogboxbody").html(message);
+    $("#dialogboxfoot").html(
+      '<button class="pure-material-button-contained active" id="btn-ok">OK</button>'
+    );
+
+    // Add an event listener to the "OK" button with ID "btn-ok"
+    $("#btn-ok").click(
+      function () {
+        this.ok(); // Call the ok function when the "OK" button is clicked
+      }.bind(this)
+    );
+  };
+
+  this.ok = function () {
+    $("#dialogbox, #dialogoverlay").remove();
+  };
+}
